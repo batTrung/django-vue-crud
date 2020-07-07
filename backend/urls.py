5a7cf4d78ce20ab8bfac68e4c5bee7e89f0de2a4
batTrung <trungbat.pv@gmail.com>
2020-07-07 12:58:29 +0700

    add url for vue app from djago urls

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('api/v1/', include('apps.core.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
+
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
